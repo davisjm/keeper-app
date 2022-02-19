9d911936e0251ff8e38e525c61eafae0c29ae6ed
Davis <Davis@Davis-MacBook-Air-3.local>
2022-02-19 14:51:21 +0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer (){
+
+    const copyrightYear = new Date().getFullYear();
+    return (
+    <footer> 
+    <p>Copyright © {copyrightYear}</p>
+    </footer>);
+}
+export default Footer;
